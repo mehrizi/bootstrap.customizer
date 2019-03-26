f592114d18344abdb4da840d3cf1796d348c182f
mahdi.me <mahdi.mehrizi@gmail.com>
2019-03-26 15:03:03 +0430

    added include paths added namespacing capability with prefix/suffix generalization fixed caching to consider file includes as well

--- src/customizer.php
+++ src/customizer.php
@@ -1,26 +1,50 @@
 <?php
+
 namespace Mehrizi\Bootstrap;
-if (file_exists(__DIR__."/../vendor"))
+if ( file_exists ( __DIR__ . "/../vendor" ) )
 {
-	define("BCUSTOMIZER_VENDOR_REL_PATH","/vendor");
+	define ( "BCUSTOMIZER_VENDOR_REL_PATH", "/vendor" );
 }
 else
-	define("BCUSTOMIZER_VENDOR_REL_PATH","/../..");
-include_once __DIR__."/..".BCUSTOMIZER_VENDOR_REL_PATH."/autoload.php";
+	define ( "BCUSTOMIZER_VENDOR_REL_PATH", "/../.." );
+include_once __DIR__ . "/.." . BCUSTOMIZER_VENDOR_REL_PATH . "/autoload.php";
 
 class Customizer
 {
-	var $variables_str = '';
+	var $variables_str        = '';
+	var $include_paths        = [];
+	var $include_paths_before = [];
+	var $prefix               = '';
+	var $suffix               = '';
 
 	/**
 	 * Customizer constructor.
 	 * @param array $variables
 	 */
-	public function __construct ( $variables = [] )
+	public function __construct ( $variables = [], $include_paths = [] )
 	{
+		if ( isset( $variables[ "_prefix" ] ) )
+		{
+			$this->prefix = $variables[ "_prefix" ];
+			unset( $variables[ "_prefix" ] );
+		}
+		if ( isset( $variables[ "_suffix" ] ) )
+		{
+			$this->suffix = $variables[ "_suffix" ];
+			unset( $variables[ "_suffix" ] );
+		}
 		foreach ( $variables as $var => $value )
 		{
 			$this->variables_str .= "$" . $var . ": " . $value . " ;";
+		}
+		$this->include_paths = $include_paths;
+		if ( isset( $this->includes[ "_before" ] ) )
+		{
+			if ( !is_array ( $this->includes[ "_before" ] ) )
+				$this->includes[ "_before" ] = [ $this->includes[ "_before" ] ];
+
+			$this->include_paths_before = $this->includes[ "_before" ];
+			unset( $this->includes[ "_before" ] );
 		}
 	}
 
@@ -30,60 +54,75 @@ class Customizer
 	 * @param bool $cache
 	 * @return bool|false|string
 	 */
-	public function build ( $echo = false, $minified = true , $cache=true)
+	public function build ( $echo = false, $minified = true, $cache = true )
 	{
-		$unique = sha1 ($this->variables_str);
-		$cache_path = __DIR__."/../cache/$unique.css";
-		if ($cache &&file_exists ($cache_path)) // read from cache if exists
+		$unique     = sha1 ( json_encode ( [ $this->variables_str, $this->include_paths, $this->include_paths_before, $this->prefix, $this->suffix ] ) );
+		$cache_path = __DIR__ . "/../cache/$unique.css";
+		if ( $cache && file_exists ( $cache_path ) ) // read from cache if exists
 		{
-			$content = file_get_contents ($cache_path);
+			$content = file_get_contents ( $cache_path );
 		}
 		else
 		{
-			$str  = "";
+			$str  = '';
 			$scss = new \Leafo\ScssPhp\Compiler();
 			if ( $minified )
 				$scss->setFormatter ( "Leafo\ScssPhp\Formatter\Crunched" );
 
-			$scss->setImportPaths (__DIR__."/../");
+			$scss->setImportPaths ( __DIR__ . "/../" );
+
 			// before bootstrap imports
-			$path  = __DIR__."/scss/_before/";
+			$path  = __DIR__ . "/scss/_before/";
 			$files = glob ( $path . "*.scss" );
 			foreach ( $files as $file )
-				$str .= "@import \"scss/_before/".basename($file)."\";";
+				$str .= "@import \"scss/_before/" . basename ( $file ) . "\";";
+
+			if ( count ( $this->include_paths_before ) )
+			{
+				foreach ( $this->include_paths_before as $path )
+					$str .= "@import \"" . $path . "\";";
+			}
 
 			// Bootstrap impementation
-			$str .= $this->variables_str . "\n@import \"".BCUSTOMIZER_VENDOR_REL_PATH."/twbs/bootstrap/scss/bootstrap.scss\";";
+			$str .= $this->variables_str . "\n@import \"" . BCUSTOMIZER_VENDOR_REL_PATH . "/twbs/bootstrap/scss/bootstrap.scss\";";
 
 			// after bootstrap imports
-			$path  = __DIR__."/scss/";
+			$path  = __DIR__ . "/scss/";
 			$files = glob ( $path . "*.scss" );
 			foreach ( $files as $file )
-				$str .= "@import \"scss/".basename($file)."\";";
+				$str .= "@import \"scss/" . basename ( $file ) . "\";";
+
+			if ( count ( $this->include_paths ) )
+			{
+				foreach ( $this->include_paths as $path )
+					$str .= "@import \"" . $path . "\";";
+			}
 
 			$content = $scss->compile ( $str );
-			file_put_contents (__DIR__."/../cache/$unique.css",$content);
+			if ( !empty( $this->prefix ) )
+				$content = $scss->compile ($this->prefix . $content . $this->suffix);
+			file_put_contents ( __DIR__ . "/../cache/$unique.css", $content );
 		}
 
-		if ($echo === false)
+		if ( $echo === false )
 			return $content;
 
-		if ($echo === true)
+		if ( $echo === true )
 		{
 			echo $content;
 			return true;
 		}
 
-		file_put_contents ($echo,$content);
+		file_put_contents ( $echo, $content );
 		return true;
 
 	}
 
-	public static function clear_cache()
+	public static function clear_cache ()
 	{
-		$files = glob ( __DIR__."/../cache/*.css" );
+		$files = glob ( __DIR__ . "/../cache/*.css" );
 		foreach ( $files as $file )
-			unlink($file);
+			unlink ( $file );
 	}
 
 	/**
